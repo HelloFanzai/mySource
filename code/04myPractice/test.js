2cc9fc01171ea5c1c3b9ce805613eadd82691b89
HelloFanzai <fanzai160311@gmail.com>
2020-12-02 00:36:10 -0500

    Edited on 12/01/2020

--- code/04myPractice/test.js
+++ code/04myPractice/test.js
@@ -1,11 +1,11 @@
-function solution(A) {
-    A.pop()
-    return A
+function theA() {
+    let A=10
+    return A++
 }
 
-let theArr=[1,2,3]
 
 
-let A = new solution( theArr )
 
-console.log( A,theArr )
+theA()
+
+console.log( theA() );
