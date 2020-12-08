a87dec03909040df363533d99c36c6e9a20ead2a
HelloFanzai <fanzai160311@gmail.com>
2020-12-07 20:42:33 -0500

    Edited on 12/07/2020

--- code/04myPractice/test.js
+++ code/04myPractice/test.js
@@ -1,6 +1,22 @@
-function theA() {
-    let A=10
-    return A++
+function theA(A, B) {
+    let theLength = A.length
+    let freqs = {}
+    let MaxFreq = 0
+    let theArray =[]
+    for (let i = 0; i < theLength; i++) {
+        theArray.push([A[i],B[i]])
+    }
+
+    console.log( theArray );
 }
 
-console.log( theA() );
+// A = [2, 10, 4, 1, 4]
+// B = [4, 1, 2, 2, 5]
+
+// A = [2, 3, 2, 3, 5]
+// B = [3, 4, 2, 4, 2]
+
+A = [2, 3, 1, 3]
+B = [2, 3, 1, 3]
+
+console.log(theA(A, B));
