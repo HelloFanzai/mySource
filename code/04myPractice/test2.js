6da8b4171e649f65d8383b21f25bd869b2060bfc
HelloFanzai <fanzai160311@gmail.com>
2021-02-04 21:43:23 -0500

    Edited on 02/04/2021

--- code/04myPractice/test2.js
+++ code/04myPractice/test2.js
@@ -1,6 +1,16 @@
-function myFN(theInput) {
-    return theInput.sort((a, b) => b - a)[1]
-}
+// function myFN(theInput) {
+//     return theInput
+// }
 
-let theInput=[2,3,4,1,6,8]
-console.log( myFN(theInput) );
+// let theInput=[2,3,4,1,6,8]
+// console.log( myFN(theInput) );
+// What is equivalent of the following using React.creatElement? 
+const element = ( <h1 className="greeting!"> Hello, world! </h1> );
+
+
+const element1 = React.createElement( 
+    'h1', 
+    {className:"greeting!"},
+     'Hello, world!' 
+);
+
