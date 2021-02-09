0d50e63d472eea415e8f5bb52b5c9ec1235dec36
HelloFanzai <fanzai160311@gmail.com>
2021-02-09 00:26:45 -0500

    Edited on 02/08/2021

---
+++ code/SitaStudy/day6/nre1.js
@@ -0,0 +1,14 @@
+let test1=10;
+console.log(test1);
+let test2='hi'
+console.log(test2);
+let test3=undefined
+console.log( typeof test3);
+
+
+let theT=theTestFn()
+console.log(theT);
+
+function theTestFn() {
+    return 'haha'
+}
