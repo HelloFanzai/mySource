7ef3245c5148051565388715b906a8244bbe631d
HelloFanzai <fanzai160311@gmail.com>
2021-02-10 02:00:44 -0500

    Edited on 02/09/2021

---
+++ code/SitaStudy/week2/day1/nre1.js
@@ -0,0 +1,16 @@
+let test1=undefined
+console.log(test1==undefined);
+
+
+// let test2='hi'
+// console.log(test2);
+// let test3=undefined
+// console.log( typeof test3);
+
+
+let theT=theTestFn()
+console.log(theT);
+
+function theTestFn() {
+    return 'haha'
+}
---
+++ code/SitaStudy/week2/day2/new1.js
@@ -0,0 +1,25 @@
+for (let i = 1; i < 10; i++) {
+    for (let j = 1; j < 10; j++) {
+        // console.log(`${i} * ${j} = ${i*j}`);
+        // console.log(i,' * ',j,' = ',i*j);  
+    }
+}
+
+
+let theString='abcdef'
+for (let i = 1; i < theString.length; i++) {
+    console.log(i);
+    theString.
+}
+
+function theFn() {
+    document.getElementById('XXX').value='default value'
+}
+
+
+
+let theA=theFn()
+console.log(theA);
+
+
+
