50ed532f27f2776e642cec9d2491af432f3b0d13
HelloFanzai <fanzai160311@gmail.com>
2021-02-13 13:11:52 -0500

    Edited on 02/12/2021

---
+++ code/03JS/Js_objCount.js
@@ -0,0 +1,34 @@
+// there is an array 
+// design a function that calculate for each
+// item how many times it occurs. It should return 
+// {
+//   'banana': 1,
+// 'apple': 1,
+// ‘orange’:2
+// }
+
+function theObjCount(theP) {
+    let theResult = {}
+
+    // for (const theI of theP) {
+    //     log
+    //     theResult[theI] = theResult[theI] + 1 || 1
+    // }
+    
+    
+    for (let i = 0; i < theP.length; i++) {
+    
+        if ( theResult[ theP[i] ] ) {
+            theResult[theP[i]]++
+        }else{
+            theResult[theP[i]]=1
+        }
+        // theResult[theP[i]]=theResult[theP[i]]+1||1
+    }
+    return theResult
+}
+
+let theP = ['banana', 'apple', 'orange', 'orange']
+console.log(theObjCount(theP));
+
+
