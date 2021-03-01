e776b0b919c610762d2869e9b3f41c6976631b85
HelloFanzai <fanzai160311@gmail.com>
2021-02-28 21:53:12 -0500

    Edited on 02/28/2021

--- code/SitaStudy/week4/day5/Week 4 Assignment/todo/script.js
+++ code/SitaStudy/week4/day5/Week 4 Assignment/todo/script.js
@@ -5,68 +5,173 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 
+document.addEventListener('keyup',function (theE) {
+  if (theE.key==' ') {
+    return newTodo()
+  }
+  
+})
+
 
 
 function newTodo() {
-  // let theUserInput = prompt('随便输入', '预输入的值')
-  // console.log(theUserInput);
-  // let theData=theUserInput
-  document.getElementById('theMainInput').style.display='block'
- 
-  // let theA=document.getElementById('theMainInput')
+  document.getElementById('theUserInput').value = ''
+  document.getElementById('theMainInput').style.display = 'block'
+  document.getElementById('theUserInput').focus()
+  theCount()
 
-  // console.log( document.getElementById('theMainInput') );
-  var tempLi = document.createElement("li");
-  var tempBtn = document.createElement("button");
-  var tempSpan = document.createElement("span");
-  var oUl = document.getElementsByTagName("ul")[0];
+  let the1snInput = document.getElementById('theMainInput')
+  the1snInput.onkeydown = function(theE){
+    if (theE.key == 'Enter') {
+      return theEnter()
+    }
+  }
 
-  tempBtn.innerHTML = "delete";
-  tempBtn.name = "delete";
 
-  // tempSpan.innerHTML = theData;
 
-  tempLi.setAttribute('class','todo-list-li')
-  tempLi.appendChild(tempSpan);
-  tempLi.appendChild(tempBtn);
+
+
+  // the1snInput.addEventListener('keyup', function (theE) {
+
+  //   if (theE.key=='Enter') {
+  //     return theEnter()
+  //   }
+
+  // })
+
+}
+
+// document.getElementById('item-count').innerHTML = document.getElementById('todo-list').children.length - 1
+
+
+
+
+
+
+function theEnter() {
+
+
+  let theUserInput = document.getElementById('theUserInput').value
+
+
+
+  let tempLi = document.createElement("li");
+  let tempCheckBox = document.createElement("input");
+  let tempInput = document.createElement("input");
+  let tempEdit = document.createElement("button");
+  let tempDel = document.createElement("button");
+  let oUl = document.getElementsByTagName("ul")[0];
+
+  tempEdit.innerHTML = "Edit";
+  tempDel.innerHTML = "Delete";
+  tempEdit.name = "Edit";
+  tempDel.name = "delete";
+  tempCheckBox.name = 'theCheckbox'
+  tempCheckBox.type = 'checkBox'
+  tempInput.value = theUserInput
+  tempInput.disabled = true
+
+
+
+  tempLi.setAttribute('class', 'todo-list-li')
+  tempLi.appendChild(tempCheckBox)
+  tempLi.appendChild(tempInput);
+  tempLi.appendChild(tempEdit);
+  tempLi.appendChild(tempDel);
+
+  if (theUserInput.trim() == '' && !confirm('Empty OK?')) {
+    return document.getElementById("theUserInput").focus()
+  }
+
   oUl.appendChild(tempLi);
+  document.getElementById('theMainInput').style.display = 'none'
 
-  // console.log(theData);
+  theCount()
+}
 
+function theTest22(theE) {
+  let theDis = document.getElementById('theInputTest')
+  if (theDis.disabled) {
+    // console.log('if');
+    theDis.disabled = false
+    document.getElementById("theInputTest").focus()
+  } else {
+    // console.log('else');
+    theDis.disabled = true
+    // document.getElementById("theInputTest").focus()
+
+  }
+}
+
+
+var oUl = document.getElementsByTagName("ul")[0];
+oUl.onclick = function (event) {
+  // console.log(oUl);
+  var event = event
+  var target = event.target;
+  if (event.target.name == "delete") {
+    target.parentNode.parentNode.removeChild(target.parentNode);
+  }
+  if (event.target.name == "Edit") {
+     let theTep = event.target.parentNode.children[1]
+    if (theTep.disabled) {
+      // console.log('if');
+      theTep.disabled = false
+      theTep.focus()
+    } else {
+      // console.log('else');
+      theTep.disabled = true
+      // document.getElementById("theInputTest").focus()
+  
+    }
+  }
+  theCount()
+};
+
+function theCount() {
   document.getElementById('item-count').innerHTML = document.getElementById('todo-list').children.length - 1
+  let theCheckbox = document.getElementsByName('theCheckbox')
+  let theChecked = 0
+
+
+  theCheckbox.forEach(e => {
+    !e.checked || theChecked++
+  })
+  document.getElementById('unchecked-count').innerHTML = theCheckbox.length - theChecked
 }
 
-document.getElementById('item-count').innerHTML = document.getElementById('todo-list').children.length - 1
 
-var theCountOfList = document.querySelector('li');
+let the1svDiv = document.createElement("div")
+let the1svInput = document.createElement("input")
+let the1svBtn = document.createElement("button")
 
-
-theCountOfList.addEventListener('click', function(ev) {
-  // if (ev.target.tagName === 'li') {
-  //   ev.target.classList.toggle('checked');
-  // }
-  console.log(theCountOfList);
-} );
-
-function theShow() {
-  document.getElementById('theMainInput').style.display='none'
+the1svDiv.id='theMainInput'
+the1svDiv.class='theMainInput'
+the1svInput.id='theUserInput'
+the1svInput.placeholder='Adding new'
+the1svInput.name='input'
+the1svBtn.innerHTML='Enter'
+the1svBtn.onclick=function() {
+  theEnter()
 }
 
-function theTest22(){
-  let theDis=document.getElementById("theInputTest").disabled
-  console.log(document.getElementById("theInputTest").disabled);
-  if (theDis) {
-    console.log('if');
-    document.getElementById("theInputTest").disabled=false
-  }else{
-    console.log('else');
-    document.getElementById("theInputTest").disabled=true
-  } 
-}
+
+the1svDiv.appendChild(the1svInput)
+the1svDiv.appendChild(the1svBtn)
+document.getElementsByTagName("ul")[0].appendChild(the1svDiv)
+
+
+{/* <div id="theMainInput" class="theMainInput">
+<input id="theUserInput" type="text" placeholder="Adding new" name="input">
+<button onclick="theEnter()">Enter</button>
+</div> */}
+
+
 
 
