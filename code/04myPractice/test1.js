7cfc9dc46d4e38abbce1b11f019f27ea46445b4f
HelloFanzai <fanzai160311@gmail.com>
2021-03-30 00:15:17 -0400

    Update on 03/29/2021 v2

--- code/04myPractice/test1.js
+++ code/04myPractice/test1.js
@@ -1,6 +1,21 @@
-function theFn(theParams) {
+
+
+const theA = {'name1':123}
+function theFn1(theParams) {
+    theParams=12
+    // console.log( theParams );
     return theParams
 }
+class theFn2 {
+    constructor(theB) {
+        theB.name2 = '666'
+        this.name1 = '12333'
+        console.log(this)
+        return theB
+    }
+}
 
-let theA = 123
-console.log(theFn(theA));
+
+console.log( theFn1(theA) );
+console.log( theFn2.name );
+console.log( theA );
