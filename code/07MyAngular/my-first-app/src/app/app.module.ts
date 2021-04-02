a24becc03ed96f181d456e0c6ced18039892376a
HelloFanzai <fanzai160311@gmail.com>
2021-04-02 13:08:02 -0400

    Update on 04/02/2021

--- code/07MyAngular/my-first-app/src/app/app.module.ts
+++ code/07MyAngular/my-first-app/src/app/app.module.ts
@@ -5,12 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { Assignment1Component } from './assignment/assignment1/assignment1.component';
-import { Assignment2Component } from './assignment/assignment2/assignment2.component';
+import { ServerComponent } from './study/server/server.component';
+import { ServersComponent } from './study/servers/servers.component';
+import { SuccessAlertComponent } from './study/success-alert/success-alert.component';
+import { WarningAlertComponent } from './study/warning-alert/warning-alert.component';
+import { Assignment1Component } from './study/assignment/assignment1/assignment1.component';
+import { Assignment2Component } from './study/assignment/assignment2/assignment2.component';
+import { StudyComponent } from './study/study.component';
 
 
 
@@ -23,6 +24,7 @@ import { Assignment2Component } from './assignment/assignment2/assignment2.compo
     WarningAlertComponent,
     Assignment1Component,
     Assignment2Component,
+    StudyComponent,
    ],
   imports: [
     BrowserModule,
