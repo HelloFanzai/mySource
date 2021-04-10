a919b21b846c98e972765f4bfaf3aef80d5514d1
HelloFanzai <fanzai160311@gmail.com>
2021-04-10 00:54:05 -0400

    Update on 04/09/2021

---
+++ code/04myPractice/test3.js
@@ -0,0 +1,33 @@
+// java script program to swap even and
+// odd bits of a given number
+ 
+// Function to swap even
+// and odd bits
+function swapBits( x)
+{
+    // Get all even bits of x
+    even_bits = x & 0xAAAAAAAA;
+ 
+    // Get all odd bits of x
+    odd_bits = x & 0x55555555;
+     
+    // Right shift even bits
+    even_bits >>= 1;
+     
+    // Left shift odd bits
+    odd_bits <<= 1;
+ 
+    // Combine even and odd bits
+    return (even_bits | odd_bits);
+}
+// Driver Code
+
+let x = 43;// 00010111
+// Output is 43 (00101011)
+
+console.log(  swapBits(23)  ); 
+// This code is contributed by sravan kumar
+
+
+// https://www.geeksforgeeks.org/swap-all-odd-and-even-bits/
+ 
