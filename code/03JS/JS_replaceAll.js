31880e3f1c2899e8832d3498a9794e671663fef8
HelloFanzai <fanzai160311@gmail.com>
2021-04-21 17:31:44 -0400

    Update on 04/21/2021

--- code/03JS/JS_replaceAll.js
+++ code/03JS/JS_replaceAll.js
@@ -1,6 +1,6 @@
 function theFn(p) {
     // return p.replaceAll('c','f123')  // replaceAll只能在HTML里运行
-    return p.replace(/c/g, 'f')
+    return p.replace(/c/g, 'f')  //正则表达式
 }
 theA = 'abcbcbc'
 console.log(theFn(theA));
