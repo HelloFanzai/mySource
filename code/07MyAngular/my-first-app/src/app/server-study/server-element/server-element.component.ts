26008aeca0b54ae75550fe3c5a6884b6e5e0343c
HelloFanzai <fanzai160311@gmail.com>
2021-05-10 17:13:59 -0400

    Update on 05/10/2021

--- code/07MyAngular/my-first-app/src/app/server-study/server-element/server-element.component.ts
+++ code/07MyAngular/my-first-app/src/app/server-study/server-element/server-element.component.ts
@@ -1,23 +1,70 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { 
+  AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, 
+  Component, 
+  DoCheck, 
+  ElementRef, 
+  Input, 
+  OnChanges, OnDestroy, 
+  OnInit, SimpleChanges, 
+  ViewChild, ViewEncapsulation } from '@angular/core';
 
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation:ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
-
-  @Input('theElement') element:{type:string, name:string, content:string}
+export class ServerElementComponent implements 
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+  {
+  @Input('theElement') element: { type: string, name: string, content: string }
+  @Input() name:string
+  @ViewChild('haeading',{static:true}) header: ElementRef
 
   constructor() {
+    console.log( 'constructor called' );
+    
+  }
+  ngOnChanges(changes:SimpleChanges){
+    console.log( 'OnChanges called' );
+    console.log(changes);
     
     
-   }
+  }
 
   ngOnInit(): void {
-    console.log( this.element.name );
+    console.log('ngOnInit called');
+    console.log(this.header);
+    
+  }
+
+  
+
+  ngDoCheck(){
+    console.log('ngdocheck called');
+  }
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called ');
+  }
+  ngAfterContentChecked(){
+    console.log('AfterContentChecked called');
+  }
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called');
+  }
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called');
+  }
+  ngOnDestroy(){
+    console.log('ngOnDestroy called');
   }
 
 }
--- code/07MyAngular/my-first-app/src/app/server-study/server-study.component.ts
+++ code/07MyAngular/my-first-app/src/app/server-study/server-study.component.ts
@@ -28,6 +28,13 @@ export class ServerStudyComponent implements OnInit {
       content: theBluePrintCreated.serverContent
     });
   }
+  onChangeFirst(){
+    this.serverElements[0].name='changed!'
+  }
+  onDestroyFirst(){
+    this.serverElements.splice(0,1)
+    // this.serverElements.shift()
+  }
 
 
   constructor() { }
