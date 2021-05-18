537f34695fcd449d949bca958cd1d8c00b9968ae
HelloFanzai <fanzai160311@gmail.com>
2021-05-18 17:07:43 -0400

    Update on 05/18/2021

---
+++ code/04myPractice/292. Nim Game.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function (n) {
+    return n % 4 > 0
+    // 因为自己先走，对手也可以移动1 or 2 or 3，所以遇4必输
+};
+
+let n = 5
+console.log(canWinNim(n));
