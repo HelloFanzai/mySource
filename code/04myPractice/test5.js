2ef8c7c869a9c86e2f49a19d5c9bfd496f225adb
HelloFanzai <fanzai160311@gmail.com>
2021-06-10 17:33:57 -0400

    Update on 06/10/2021

--- code/04myPractice/test5.js
+++ code/04myPractice/test5.js
@@ -1,7 +1,12 @@
-const theArr = [2,6,8,11,13,22]
-console.log( theArr.find( (theValue, theIndex, theArr)=>{
-    console.log( theValue,theIndex,theArr );
-    if (theIndex>=3) {
+const theArr = [0, 2, 0, 0, 6, 0, 8, 11, 13, 22]
+console.log(theArr.find((theValue, theIndex, theArr) => {
+    console.log(theValue, theIndex, theArr);
+    if (theIndex >= 3) {
         return theIndex
     }
-} ) )
+}))
+
+for (let i = 0; i < theArr.length; i++) {
+    
+    
+}
