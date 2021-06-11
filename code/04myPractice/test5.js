e869c60c5b817bd5fac97393187a6ca2a6882e96
HelloFanzai <fanzai160311@gmail.com>
2021-06-10 23:24:23 -0400

    Update on 06/10/2021 v2

--- code/04myPractice/test5.js
+++ code/04myPractice/test5.js
@@ -1,12 +1,9 @@
-const theArr = [0, 2, 0, 0, 6, 0, 8, 11, 13, 22]
-console.log(theArr.find((theValue, theIndex, theArr) => {
-    console.log(theValue, theIndex, theArr);
-    if (theIndex >= 3) {
-        return theIndex
-    }
-}))
+let theThis=[0, 2, 0, 0, 6, 0, 8, 11, 13, 22]
+const theArr = theThis
+.map(theM=>{return  ++theM})
+.filter(theF=>{return theF>=5 })
 
-for (let i = 0; i < theArr.length; i++) {
-    
-    
-}
+
+
+theThis=theArr
+console.log( theThis,theArr );
