fbe4a7c45f80a0f9defacb03b9b9cca1fdf632dc
HelloFanzai <fanzai160311@gmail.com>
2021-07-08 16:55:47 -0400

    Update on 07/08/2021

--- code/04myPractice/test3.js
+++ code/04myPractice/test3.js
@@ -1,30 +1,47 @@
-
-const employees = [
-    { id: 100, name: 'Jose', managerId: 100 },
-    { id: 200, name: 'Viktar', managerId: 100 },
-    { id: 300, name: 'Pavel', managerId: 200 },
-    { id: 400, name: 'Mary', managerId: 200 },
-    { id: 500, name: 'Mary2', managerId: 400 },
-    { id: 600, name: 'Mary3', managerId: 500 },
-    { id: 700, name: 'Mary4', managerId: 500 },
-    { id: 800, name: 'Mary5', managerId: 600 },
-    { id: 900, name: 'Mary6', managerId: 700 },
-];
-
-let theTargetID = 900
-
-function theFn(theTargetID) {
-    let theAnser = []
-    let filteredData = employees.find(data => data.id == theTargetID)
-    while (filteredData.managerId != filteredData.id) {
-        filteredData = employees.find(data => data.id == theTargetID);
-        if (filteredData) {
-            theAnser.push(filteredData.managerId)
-            theTargetID = filteredData.managerId
+function checkForGraph(nodeMap, N, start, touchedPoints) {
+    const target = touchedPoints[0];
+    const paths = nodeMap[start];
+    if (!paths || !paths.length) {
+        return [];
+    }
+    for (const i of paths) {
+        if (i === target) {
+            return touchedPoints;
+        } else {
+            touchedPoints.push(i);
+            return checkForGraph(nodeMap, N, i, [...touchedPoints]);
         }
     }
-    theAnser.pop()
-    // theAnser.length--
-    return theAnser
 }
-console.log( theFn(theTargetID)  );
+
+function solution(A, B) {
+    const nodeMap = {}
+    const N = A.length;
+    for (let i in A) {
+        const a = A[i];
+        const b = B[i];
+        nodeMap[a] = nodeMap[a] || [];
+        nodeMap[a].push(b);
+    }
+    const touchedPoints = [A[0]];
+    const path = checkForGraph(nodeMap, N, A[0], [...touchedPoints]);
+    // console.log(path.length === N);
+    if (path.length === N) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// var A = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
+// var B = [39,40,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38];
+
+
+// var A = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+// var B = [29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28];
+
+
+A = [1, 2, 3]
+B = [1, 2, 3]
+console.log(solution(A, B));
