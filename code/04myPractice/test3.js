0c011e697bd90e953b5d4c9952e7dd1c98417376
HelloFanzai <fanzai160311@gmail.com>
2021-07-09 23:36:27 -0400

    Update on 07/09/2021

--- code/04myPractice/test3.js
+++ code/04myPractice/test3.js
@@ -1,47 +1,5 @@
-function checkForGraph(nodeMap, N, start, touchedPoints) {
-    const target = touchedPoints[0];
-    const paths = nodeMap[start];
-    if (!paths || !paths.length) {
-        return [];
-    }
-    for (const i of paths) {
-        if (i === target) {
-            return touchedPoints;
-        } else {
-            touchedPoints.push(i);
-            return checkForGraph(nodeMap, N, i, [...touchedPoints]);
-        }
-    }
-}
+let theArr='123'
+console.log( Array.from(theArr,n=>n*1) );
 
-function solution(A, B) {
-    const nodeMap = {}
-    const N = A.length;
-    for (let i in A) {
-        const a = A[i];
-        const b = B[i];
-        nodeMap[a] = nodeMap[a] || [];
-        nodeMap[a].push(b);
-    }
-    const touchedPoints = [A[0]];
-    const path = checkForGraph(nodeMap, N, A[0], [...touchedPoints]);
-    // console.log(path.length === N);
-    if (path.length === N) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-// var A = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
-// var B = [39,40,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38];
-
-
-// var A = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-// var B = [29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28];
-
-
-A = [1, 2, 3]
-B = [1, 2, 3]
-console.log(solution(A, B));
+let theArr2='123'
+console.log( theArr2.split('').map(n=>n/1) );
--- code/04myPractice/test4.js
+++ code/04myPractice/test4.js
@@ -1,35 +1,3 @@
-function solution(A, B) {
-    // write your code in JavaScript (Node.js 8.9.4)
-    // A=[...new Set(A)].sort((a, b) => a - b)
-    // B=[...new Set(B)].sort((a, b) => a - b)
-    // return A+''== B+''
-    let thePofA = 0
-    let thePofB = -1
-
-
-    thePofB = B.indexOf(A[0])
-    // for (let i = 0; i < A.length; i++) {
-    //     if (B.indexOf(A[i]) == thePofB) {
-    //         console.log( i,thePofB );
-    //         return false
-    //     }
-    //     thePofB++
-    //     if (thePofB > B.length-1) {
-    //         thePofB=0
-    //     }
-    // }
-    // return true
-
-
-    B=B.slice(B.indexOf(A[0]),B.length).concat(B.splice(0,B.indexOf(A[0])))
-    
-    
-    return A+'' == B+''
-    
-
-
-}
-let theA = [1, 2, 3]
-let theB = [2, 3, 1]
-console.log(solution(theA, theB));
-
+let thes1=112; 
+let theS2=[...thes1,4]; //[ '1', '1', '2' ]
+console.log(theS2);
