cabca14426c640802fbccd0ca29a5ab4fb503b6f
HelloFanzai <fanzai160311@gmail.com>
2021-10-15 22:12:23 -0700

    Update on 10/15/2021

--- code/04myPractice/test2.js
+++ code/04myPractice/test2.js
@@ -1,16 +1,4 @@
-function theFn(theA,theB) {
-    theA.unshift(theA.pop())
-    console.log( theA );
-    return theB
-}
+let theArray = []
 
-
-
-
-let theA = [5,7,4]
-console.log( theA.find(e=>e>8) );
-let theB = ['B','C','D']
-console.log(theFn(theA,theB));
-
-theN=[]
-console.log( !!theN.length );
+console.log( !!theArray ); //true
+console.log( !!theArray.length ); //false
